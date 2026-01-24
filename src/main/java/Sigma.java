4d45efe5ce8c58cd1976caec017409dc0df066b6
Alex-Chen-666 <czdtom@connect.hku.hk>
2026-01-24 11:42:02 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Sigma.java
@@ -0,0 +1,14 @@
+public class Sigma {
+    public static void main(String[] args) {
+        String horizontalLine = "____________________________________________________________";
+        String botName = "Sigma";
+
+        System.out.println(horizontalLine);
+        System.out.println(" Hello! I'm " + botName);
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
